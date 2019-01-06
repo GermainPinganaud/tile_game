aee1207a0eb937f5d38f395890247cfd1f9c620c
Germain PINGANAUD <germain.pinganaud@gmail.com>
2019-01-06 11:45:42 +0100

    cleaning comment

--- app.js
+++ app.js
@@ -8,24 +8,3 @@ window.onload = function() {
   selectTile.context.drawImage(tile, 0, 0);
   console.log(selectTile.context.test());
 };
-
-
-
-
-
-
-
-
-
-//
-// class Context {
-//   // constructor
-//   constructor (canvasId) {
-//     this.canvas = document.getElementById(canvasId)
-//     this.context = this.canvas.getContext('2d')
-//   }
-// // methods
-//
-// }
-// // instanciation
-// var selectTile = new Context("select-tile");
