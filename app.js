e7dac661aef29b4c166b3516e4f598b27f663e1c
Germain PINGANAUD <germain.pinganaud@gmail.com>
2019-01-06 14:33:47 +0100

    minor changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // ne fonctionne pas coté client
+var http = require('http');
 var selectTile = require("./context");
 
 var tile = new Image();
@@ -8,3 +9,24 @@ window.onload = function() {
   selectTile.context.drawImage(tile, 0, 0);
   console.log(selectTile.context.test());
 };
+
+
+
+
+
+
+
+
+
+//
+// class Context {
+//   // constructor
+//   constructor (canvasId) {
+//     this.canvas = document.getElementById(canvasId)
+//     this.context = this.canvas.getContext('2d')
+//   }
+// // methods
+//
+// }
+// // instanciation
+// var selectTile = new Context("select-tile");
